dbf51cb430d6dd568795c32e5fe40e1f6d9972d8
Mohammad Mahdi Daghighi <mohammadmahdi.daghighi81@gmail.com>
2022-11-23 20:17:16 +0330

    seeders completed

--- src/seeders/20221123140057-demo-categories.js
+++ src/seeders/20221123140057-demo-categories.js
@@ -11,6 +11,61 @@ module.exports = {
           created_at: new Date(),
           updated_at: new Date(),
         },
+        {
+          name: 'روانشناسی',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'نمایشنامه',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'تاریخ',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'شعر',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: ' علوم اجتماعی',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'داستان و رمان',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'مدیریت، بازاریابی و فروش',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'سبک زندگی',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'موسیقی',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'دین و عرفان',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'زندگی‌نامه ، سفرنامه و خاطرات',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
       ],
       {},
     );
