4d4618a214176e0171d7ed7f237ac4cd394e94be
Mehdi Maleki <malekeym@gmail.com>
2023-01-07 16:10:02 +0330

    whiteListing uploaded books

--- src/controllers/book.controller.ts
+++ src/controllers/book.controller.ts
@@ -40,7 +40,8 @@ class BookController {
         this.bookUserService.hasBook(bookId, req.user.id),
         this.cartService.hasBook(bookId, req.user.id),
       ]);
-      const downloadUrl = isBuyed ? await this.booksService.getFileDownloadUrl(findOneBookData.id) : null;
+      const isInWhiteList = [1, 2, 3, 4, 6, 9, 15, 41].includes(bookId);
+      const downloadUrl = isBuyed && isInWhiteList ? await this.booksService.getFileDownloadUrl(findOneBookData.id) : null;
       res.status(200).json({ data: { ...findOneBookData, isBuyed, isInCart, downloadUrl }, message: 'findOne' });
     } catch (error) {
       next(error);
