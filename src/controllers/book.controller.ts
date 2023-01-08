d236a27701d3786f48963ad0e63f695a229ced87
Mehdi Maleki <malekeym@gmail.com>
2023-01-08 20:02:26 +0330

    chore: whitelist

--- src/controllers/book.controller.ts
+++ src/controllers/book.controller.ts
@@ -40,7 +40,9 @@ class BookController {
         this.bookUserService.hasBook(bookId, req.user.id),
         this.cartService.hasBook(bookId, req.user.id),
       ]);
-      const isInWhiteList = [1, 2, 3, 4, 6, 9, 15, 41].includes(bookId);
+      const isInWhiteList = [
+        1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 41, 48, 49, 50, 51, 52,
+      ].includes(bookId);
       const downloadUrl = isBuyed && isInWhiteList ? await this.booksService.getFileDownloadUrl(findOneBookData.id) : null;
       res.status(200).json({ data: { ...findOneBookData, isBuyed, isInCart, downloadUrl }, message: 'findOne' });
     } catch (error) {
